7962e49ecf92e67764d3c77c215aa9e0c672711c
Matteo Taroli <taroli.matteo@gmail.com>
2015-05-15 14:11:32 +0200

    Basic functions OK

--- todo.pl
+++ todo.pl
@@ -1,6 +1,9 @@
+package TODO 1;
+
 use strict;
 use warnings;
 
+sub main;
 sub showTodo;
 sub readTodo;
 sub writeTodo;
@@ -15,20 +18,34 @@ my $fh;
 my %todo;
 readTodo;
 
-my $argCount = $#ARGV + 1;
-if ($argCount == 0) { # No args -> Show todo
-	showTodo;
-} elsif ($argCount == 3) { # Args -> if todo (add | del) topic task -> modify todo
+main;
 
-} else {
-		print "Usage :\n\t$0\n\t$0 (add | del) topic task\n";
-}
-# Args -> Too many / Too few -> quit
 writeTodo;
+1;
 
 # # Subroutines
 # # # # # # # # # # # # # # # # # #
+sub main {
+	my $argCount = $#ARGV + 1;
+	if ($argCount == 0) { # No args -> Show todo
+		showTodo;
+	} elsif ($argCount == 3) {
+		if (lc($ARGV[0]) eq "add") {
+			addToTodo($ARGV[1], $ARGV[2]);
+		} elsif (lc($ARGV[0]) eq "del") {
+			delFromTodo($ARGV[1], $ARGV[2]);
+		} else {
+			print "Usage :\n\t$0\n\t$0 (add | del) topic task\n";
+		}
+	} else {
+		print "Usage :\n\t$0\n\t$0 (add | del) topic task\n";
+	}
+	return;
+}
+
 sub showTodo {
+	print "Todo list\n#####################\n";
+	print "No task yet.\n" unless keys(%todo) > 0;
 	foreach my $topic (sort keys %todo) {
 		print "$topic\n";
 		for my $i (0 .. $#{$todo{$topic}}) {
@@ -39,24 +56,20 @@ sub showTodo {
 }
 
 sub readTodo {
-	# If file exists, open it in R/W, if not, create and open in R
 	if (-e $file) {
-		open ($fh, '+<', $file) or die "Couldn't open the file\n";
-	} else {
-		open ($fh, '>', $file) or die "Couldn't open the file\n";
+		open ($fh, '<', $file) or die "Couldn't open the file for reading\n";
+		while (my $line = <$fh>) {
+			my ($who, $rest) = split /:\s*/, $line, 2;
+			my @fields = split ' ', $rest;
+			$todo{$who} = [ @fields ];
+		}
+		close($fh);
 	}
-
-	while (my $line = <$fh>) {
-		my ($who, $rest) = split /:\s*/, $line, 2;
-		my @fields = split ' ', $rest;
-		$todo{$who} = [ @fields ];
-	}
-	close($fh);
 	return;
 }
 
 sub writeTodo {
-	open ($fh, '>', $file) or die "Couldn't open the file\n";
+	open ($fh, '>', $file) or die "Couldn't open the file for writing\n";
 
 	foreach my $topic (sort keys %todo) {
 		print $fh "$topic:";
@@ -69,11 +82,33 @@ sub writeTodo {
 	return;
 }
 
-sub addToTodo { # If topic exists add to topic, else create topic
+sub addToTodo {
+	my ($topic, $task) = @_;
+	$topic = uc($topic);
+	$task = lc($task);
 
+	if (exists $todo{$topic}) {
+		push($todo{$topic}, $task);
+	} else {
+		$todo{$topic} = [$task];
+	}
+	showTodo;
 	return;
 }
 
-sub delFromTodo { # If topic and task exists, delete task, if topic is left without task, remove it, else do nothing
+sub delFromTodo {
+	my ($topic, $task) = @_;
+	$topic = uc($topic);
+	$task = lc($task);
+
+	if (exists $todo{$topic}) {
+		my $index ;
+		for ($index = 0; $index < @{$todo{$topic}}; $index++) {
+			last if ($todo{$topic}[$index] eq $task);
+		}
+		splice($todo{$topic}, $index, 1);
+		delete($todo{$topic}) unless (@{$todo{$topic}} != 0);
+	}
+	showTodo;
 	return;
 }
